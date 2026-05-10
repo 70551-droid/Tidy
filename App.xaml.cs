b070f5a738263a17db43fe3c46842a34142f9bfc
70551-droid <70551@darseburhani.org>
2026-05-10 20:32:07 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,5 +4,12 @@ namespace Tidy
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+            window.Show();
+        }
     }
 }
